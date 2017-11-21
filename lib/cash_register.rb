dc82aa84de5a0b5144e38c7903109ad3bdccdd2a
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-21 18:21:16 -0500

    Complete first half of lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
 
+attr_writer :discount, :total
+attr_reader :discount, :total
+
+def initialize(discount=0)
+  @total = 0
+  @discount = discount
+end
+
+def add_item(item, price, quantity=1)
+   self.total += (price * quantity)
+end
+
+def apply_discount
+  self.total = self.total.to_i * (1-discount/100)
+end
+
+end
