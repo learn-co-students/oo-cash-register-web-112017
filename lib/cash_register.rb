0da078472096be0d0f51f2ec62836bb97f442fb2
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-22 00:20:13 -0500

    complete cash register class lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,19 +1,32 @@
 class CashRegister
 
-attr_writer :discount, :total
-attr_reader :discount, :total
+attr_accessor :discount, :total, :items, :last_transaction
 
 def initialize(discount=0)
   @total = 0
   @discount = discount
+  @items = []
 end
 
 def add_item(item, price, quantity=1)
    self.total += (price * quantity)
+    quantity.times do
+     self.items << item
+     end
+    self.last_transaction = price * quantity
 end
 
 def apply_discount
-  self.total = self.total.to_i * (1-discount/100)
+  if discount == 0
+    "There is no discount to apply."
+  else
+    self.total = (self.total * (1 - discount/100.to_f)).to_i
+    "After the discount, the total comes to $#{self.total}."
+  end
 end
+
+def void_last_transaction
+   self.total -= self.last_transaction
+ end
 
 end
