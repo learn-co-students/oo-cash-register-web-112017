ebf62a380483a30e650c1638913b34d51dbbc6c5
iamm28 <alisonmackay246@gmail.com>
2017-11-21 21:10:49 -0500

    made cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require "pry"
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(*discount)
+    @total = 0
+    @discount = discount[0]
+    @items = []
+  end
+
+  def add_item(title,price,*quantity)
+    quantity = quantity[0]
+    if quantity == nil
+      @total += price
+      @items << title
+    else
+      @total += price*quantity
+      quantity.times do @items << title end
+    end
+    @last_transaction = price
+  end
+
+  def apply_discount
+    if @discount == nil
+      "There is no discount to apply."
+    else
+      dis = @total*(@discount.to_f/100)
+      @total -= dis
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total -=@last_transaction
+  end
+end
